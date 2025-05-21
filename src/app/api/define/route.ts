0217cd4a40d8ccbc1c0730d1630a422c1993afa5
Vincent Lin <linv@mit.edu>
2025-05-21 17:44:03 -0400

    add part of speech saving

--- src/app/api/define/route.ts
+++ src/app/api/define/route.ts
@@ -12,7 +12,7 @@ export async function GET(req: NextRequest) {
     try {
         const res = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
         const definitions = res.data[0].meanings.flatMap((m: any) =>
-            m.definitions.map((d: any) => d.definition)
+            m.definitions.map((d: any) => `${m.partOfSpeech}: ${d.definition}`)
         );
         return NextResponse.json({ word, definitions });
     } catch (err: any) {
--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -5,19 +5,26 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_WORDS_DB_ID as string;
 
 export async function POST(req: NextRequest) {
-    const { word, definition, book_title } = await req.json();
+    const { word, fullDefinition, bookTitle } = await req.json();
 
-    if (!word || !definition) {
+    if (!word || !fullDefinition) {
         return NextResponse.json({ error: "Missing data" }, { status: 400 });
     }
+
+    // definition actually contains part of speech and definition:
+    // <pos>: <definition>
+    const splitIx = fullDefinition.indexOf(": ");
+    const partOfSpeech = fullDefinition.slice(0, splitIx).trim();
+    const definition = fullDefinition.slice(splitIx + 2).trim(); // account for colon and space
 
     try {
         await notion.pages.create({
             parent: { database_id: databaseId },
             properties: {
                 Word: { title: [{ text: { content: word } }] },
+                "Part Of Speech": { rich_text: [{ text: { content: partOfSpeech } }] },
                 Definition: { rich_text: [{ text: { content: definition } }] },
-                Book: { rich_text: [{ text: { content: book_title } }] },
+                Book: { rich_text: [{ text: { content: bookTitle } }] },
             },
         });
 
